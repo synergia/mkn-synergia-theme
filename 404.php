a0da09884db7d47aabead7cdf820ae18355cfce2
stsdc <s.dac@yandex.ru>
2014-09-20 12:28:14 +0200

    init 2

--- 404.php
+++ 404.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
 
-<<<<<<< HEAD
 <?php get_template_part('template-part', 'head'); ?>
 
 <?php get_template_part('template-part', 'topnav'); ?>
@@ -22,33 +21,3 @@
     <!-- end content container -->
 
 <?php get_footer(); ?>
-=======
-<?php
-//allows the theme to get info from the theme options page
-global $options;
-foreach ($options as $value) {
-    if (get_settings( $value['id'] ) === FALSE) { $$value['id'] = $value['std']; } else { $$value['id'] = get_settings( $value['id'] ); }
-}
-?>
-
-	<div id="content">
-
-			<div class="postsingle" id="post-<?php the_ID(); ?>">
-			
-				<h1>Whoops, can't found this page!</h1>
-				
-				<div class="entry">
-					<p>Sorry, we can't find what you're looking for.</p>
-					
-				</div>
-
-			</div>
-
-
-	</div>
-
-
-
-<?php get_sidebar('standard'); ?>
-<?php get_footer(); ?>
->>>>>>> 938bfb5e6792896f1272c09c3564f2a286fb3231
--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
     <div class="dmbs-footer">
         <?php
             global $dm_settings;
@@ -15,21 +14,5 @@
 <!-- end main container -->
 
 <?php wp_footer(); ?>
-=======
-</div> <!-- END #container -->
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-9997784-9']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
->>>>>>> 938bfb5e6792896f1272c09c3564f2a286fb3231
 </body>
 </html>
