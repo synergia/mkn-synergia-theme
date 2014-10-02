3e8e4419fa176114a47c4bef7a40e445d5a2263f
stsdc <s.dac@yandex.ru>
2014-10-02 22:12:23 +0200

    Chyba zrobiłem stopkę

--- footer.php
+++ footer.php
@@ -1,12 +1,25 @@
     <div class="dmbs-footer">
                 <div class="row">
-                    <div class="mkn-footer col-xs-5">Międzywydziałowe Koło Naukowe "Synergia" <?php echo comicpress_copyright(); ?>
+                    <div class="mkn-footer col-sm-5">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">Międzywydziałowe Koło Naukowe "Synergia"</a> <?php echo comicpress_copyright(); ?>
+                        <ul>
+                            <li><a href="http://tmm.pwr.wroc.pl">Zakład Teorii Maszyn i Układów Mechatronicznych</a></li>
+                            <li><a href="http://wmech.pwr.wroc.pl/">Wydział Mechaniczny</a></li>
+                            <li><a href="http://weka.pwr.wroc.pl/">Wydział Elektroninki</a></li>
+                        </ul>
                             <?php get_template_part('template-part', 'footernav'); ?>
 
                     </div>
-                    <a href="http://pwr.wroc.pl" title="Seks, muzyka, Po-li-technika!" target="_blank"><div class="col-xs-2 pwr"><img src="<?php echo get_template_directory_uri(); ?>/img/pwr.png"/>
+                    <a href="http://pwr.wroc.pl" title="Seks! Muzyka! Po-li-technika!" target="_blank"><div class="col-sm-2 pwr"><img src="<?php echo get_template_directory_uri(); ?>/img/pwr.png"/>
                 </div></a>
-                    <div class="col-xs-5 footer-right"><a href="http://127.0.0.1/synergia/?page_id=66">Archiwum</a></div>
+                                                            <div class="col-sm-5 footer-right">
+                                            <div class="center">
+                        <a href="http://127.0.0.1/synergia/?page_id=66" title="Archiwum"><i class="icon-archive"></i></a>
+                        <a href="http://facebook.com/mknmsynergia" title="Polub"><i class="icon-facebook"></i></a>
+                        <a href="http://github.com/synergia" title="Kod"><i class="icon-github"></i></a>
+                                            </div>
+                    </div>
+
 
     </div>
 
