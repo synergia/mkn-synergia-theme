4048384d797d3f278276577aa061a8e957ff6919
stsdc <s.dac@yandex.ru>
2014-10-11 17:27:41 +0200

    Ficze dla Safari i Opery

--- header.php
+++ header.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+<!--Design&Developing: Stanisław Dac-->
 <head>
     <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/img/safari_60.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_directory'); ?>/img/safari_76.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_directory'); ?>/img/safari_120.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_directory'); ?>/img/safari_152.png">
     <meta name="description" content="<?php echo esc_attr(get_bloginfo('description')); ?>" />
-    <title><?php wp_title('&laquo;', true, 'right'); ?></title>
+    <title>
+        <?php wp_title( '&laquo;', true, 'right'); ?>
+    </title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
-<div class="container dmbs-container">
+    <div class="container dmbs-container">
---
+++ readme.txt
@@ -0,0 +1,11 @@
+Nanote one-file simple blog script 
+2010, Zorg (ekumena@gmail.com), GPL v.3
+
+Для установки:
+
+ 1 - закачайте содержимое архива на сервер;
+ 2 - установите права (chmod) на /data и все содержащиеся в ней файлы - 0777
+
+Для включения ЧПУ:
+
+ 1 - переименуйте rename.htaccess в .htaccess
