201a6fd83fb0ac209dd0c9d3e90f33aeb1ecfafb
stsdc <s.dac@yandex.ru>
2014-10-23 23:05:16 +0200

    hotfixes

--- template-part-sponsorowane.php
+++ template-part-sponsorowane.php
@@ -1,30 +1,3 @@
-    <div class="sponsorowane-left col-sm-12">
-        <h3>Sponsorzy</h3>
-
-<?php
-    $args = array(
-        'post_type' => 'sponsorowane ',
-        'category_name' => 'sponsorzy'
-    );
-    $products = new WP_Query( $args );
-    if( $products->have_posts() ) {
-      while( $products->have_posts() ) {
-        $products->the_post();
-        ?>
-            <div class="sponsorowane">
-                <a href="<?php the_title(); ?>">
-				<?php the_post_thumbnail('full'); ?>
-                </a>
-            </div>
-<?php
-      }
-    }
-    else {
-      echo 'Nic a nic';
-    }
-  ?>
-    </div>
-
     <div class="wspolpraca-left col-sm-12">
         <h3>Współpraca</h3>
 
@@ -52,3 +25,30 @@
     }
   ?>
     </div>
+<div class="sponsorowane-left col-sm-12">
+        <h3>Sponsorzy</h3>
+
+<?php
+    $args = array(
+        'post_type' => 'sponsorowane ',
+        'category_name' => 'sponsorzy'
+    );
+    $products = new WP_Query( $args );
+    if( $products->have_posts() ) {
+      while( $products->have_posts() ) {
+        $products->the_post();
+        ?>
+            <div class="sponsorowane">
+                <a href="<?php the_title(); ?>">
+				<?php the_post_thumbnail('full'); ?>
+                </a>
+            </div>
+<?php
+      }
+    }
+    else {
+      echo 'Nic a nic';
+    }
+  ?>
+    </div>
+
