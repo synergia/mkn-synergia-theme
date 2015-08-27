419a2b20b7e1b9c8ae5538c99900819acb6f4067
stsdc <s.dac@yandex.ru>
2015-08-27 21:38:07 +0200

    refactoring topbar

--- template-part-head.php
+++ template-part-head.php
@@ -2,22 +2,5 @@
 
 <div class="container dmbs-container">
 
-<?php if ($dm_settings['show_header'] != 0) : ?>
-
-    <div class="row dmbs-header">
-
-
-        <div class="col-md-8 dmbs-header-text">
-            <?php if ( get_header_textcolor() != 'blank' ) : ?>
-                <h1><a class="custom-header-text-color" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-                <h4 class="custom-header-text-color"><?php bloginfo( 'description' ); ?></h4>
-            <?php endif; ?>
-            <?php else : ?>
-                <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-                <h4><?php bloginfo( 'description' ); ?></h4>
-            <?php endif; ?>
-        </div>
-
-    </div>
 
 <?php endif; ?>
--- template-part-topnav.php
+++ template-part-topnav.php
@@ -21,7 +21,7 @@
                         'depth'             => 2,
                         'container'         => 'div',
                         'container_class'   => 'collapse navbar-collapse navbar-1-collapse',
-                        'menu_class'        => 'nav navbar-nav',
+                        'menu_class'        => 'navbar-nav',
                         'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
                         'walker'            => new wp_bootstrap_navwalker())
                 );
