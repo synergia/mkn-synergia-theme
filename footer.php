5160c8f42b99ea7fac542e37864c6178210afb6d
stsdc <s.dac@yandex.ru>
2015-08-29 00:05:02 +0200

    footer sassed. breakpointy dla gridlayout, ikony w osobnym pliku

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php global $dm_settings; ?>
-<div class="dmbs-footer row">
-                    <div class="mkn-footer col-sm-5">
-                        <div class="mkn-footer-inner">
+<div class="dmbs-footer">
+    <div class=" gl">
+                    <div class="mknm-footer gl-sm-5 gl-cell">
                             <a href="<?php echo esc_url( home_url( '/' ) ); ?>">Międzywydziałowe Koło Naukowe<br>Mechatroniki "Synergia"</a> <?php echo comicpress_copyright(); ?>
                             <ul>
                                 <li><a href="http://tmm.pwr.wroc.pl">Zakład Teorii Maszyn i Układów Mechatronicznych</a></li>
@@ -10,17 +10,21 @@
                             </ul>
                             <?php get_template_part('template-part', 'footernav'); ?>
 
+                    </div>
+                    <div class="gl-sm-2 gl-cell">
+                        <a href="http://pwr.wroc.pl" target="_blank">
+                            <div class="gl-sm-2 gl-cell pwr"><img src="<?php echo get_template_directory_uri(); ?>/img/pwr.png"/>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="gl-sm-5 gl-cell footer-right">
+                        <div class="center">
+                            <a href="<?php echo $dm_settings['archiwum'] ?>" title="Archiwum"><i class="icon-archive"></i></a>
+                            <a href="http://facebook.com/mknmsynergia" title="Polub"><i class="icon-facebook"></i></a>
+                            <a href="http://github.com/synergia" title="Kod"><i class="icon-github"></i></a>
                         </div>
                     </div>
-                    <a href="http://pwr.wroc.pl" target="_blank"><div class="col-sm-2 pwr"><img src="<?php echo get_template_directory_uri(); ?>/img/pwr.png"/>
-                </div></a>
-                                                            <div class="col-sm-5 footer-right">
-                                            <div class="center">
-                        <a href="<?php echo $dm_settings['archiwum'] ?>" title="Archiwum"><i class="icon-archive"></i></a>
-                        <a href="http://facebook.com/mknmsynergia" title="Polub"><i class="icon-facebook"></i></a>
-                        <a href="http://github.com/synergia" title="Kod"><i class="icon-github"></i></a>
-                                            </div>
-                    </div>
+    </div>
 </div>
 <!-- end main container -->
 
