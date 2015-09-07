dcfaca0adf6222b2f9559f551bcff2e20c00604d
stsdc <s.dac@yandex.ru>
2015-09-07 02:04:52 +0200

    dodano wkładki

--- author.php
+++ author.php
@@ -10,7 +10,7 @@
     $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
     ?>
     <div class="gl-md-9 gl-cell">
-        <div class="gl">
+        <div class="gl usercard">
             <div class="gl-md-3 userpic gl-cell">
                 <img src="<?php echo $curauth->image; ?>"/>
             </div>
@@ -25,38 +25,59 @@
             </div>
         </div>
 
-    <!-- The Loop -->
-    <div class="post-list">
-    <?php
-        $args = array(
-            'post_type' => 'projekt ',
-            'author' => $curauth->ID
-           );
-        $products = new WP_Query( $args );
-        if( $products->have_posts() ) {
-          while( $products->have_posts() ) {
-            $products->the_post();
-            ?>
-              <div class="post-list-item ">
-                <!-- <div class="gl-md-3 gl-cell"> -->
-                  <?php the_post_thumbnail("thumbnail"); ?>
-                <!-- </div> -->
-                <div class="post-list-item-content">
-                  <a href="<?php the_permalink(); ?>">
-                    <h2><?php the_title(); ?></h2>
-                  </a>
-                  <div class="excerpt">
-                    <?php the_excerpt(); ?>
-                  </div>
-                </div>
-              </div>
-    <?php
-          }
-        }
-        else {
-          echo 'Nic a nic';
-        }
-      ?>
+        <div class="tabs">
+          <input id="tab-1" name="tabset-1" type="radio" hidden checked/>
+          <input id="tab-2" name="tabset-1" type="radio" hidden />
+          <nav class="tabs-nav" role="navigation">
+            <input id="tabsnav-1" type="checkbox" hidden />
+            <label for="tabsnav-1">☰</label>
+            <ul>
+              <li><label for="tab-1">Projekty</label></li>
+              <li><label for="tab-2">Github</label></li>
+
+            </ul>
+          </nav>
+          <div class="tab">
+            <div class="post-list">
+            <?php
+                $args = array(
+                    'post_type' => 'projekt ',
+                    'author' => $curauth->ID
+                   );
+                $products = new WP_Query( $args );
+                if( $products->have_posts() ) {
+                  while( $products->have_posts() ) {
+                    $products->the_post();
+                    ?>
+                      <div class="post-list-item ">
+                        <!-- <div class="gl-md-3 gl-cell"> -->
+                          <?php the_post_thumbnail("thumbnail"); ?>
+                        <!-- </div> -->
+                        <div class="post-list-item-content">
+                          <a rel="bookmark" href="<?php the_permalink(); ?>">
+                            <h2><?php the_title(); ?></h2>
+                          </a>
+                          <div class="excerpt">
+                            <?php the_excerpt(); ?>
+                          </div>
+                        </div>
+                      </div>
+            <?php
+                  }
+                }
+                else {
+                  echo 'Nic a nic';
+                }
+              ?>
+          </div>
+        </div>
+          <div class="tab">
+            <h2>Second tab</h2>
+            <p>Hellou.</p>
+          </div>
+
+
+
     </div>
 
     <!-- End Loop -->
--- js/js.js
+++ js/js.js
@@ -280,6 +280,16 @@ jQuery(function($) {
     Plugin.call($target, option);
   });
 
+//TABS
+    $(function() {
+      // set active radio to address bar
+      $(document).on('click', '.tabs .tabs-nav label', function() {
+        var hash = '#' + $(this).attr('for');
+        window.history.replaceState('', '', hash);
+      });
+      // select active radio based on hash
+      $(document.location.hash).prop('checked', true);
+    });
 });
 
 //AUTHOR
