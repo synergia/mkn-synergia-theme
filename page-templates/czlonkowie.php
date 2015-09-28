24a5bc6bd829bfc9f22c7042566f9ef1c206305c
stsdc <s.dac@yandex.ru>
2015-09-28 21:11:09 +0200

    wyświetlanie członków z ilością postów > 0

--- page-templates/czlonkowie.php
+++ page-templates/czlonkowie.php
@@ -16,9 +16,9 @@ Template Name: Członkowie
     <div class="gl-sm-9 gl-cell">
 
 <?php
-
+// https://tommcfarlin.com/wp_user_query-multiple-roles/
 // prepare arguments
-$args = array (
+$synergia_member_args = array (
 	'role'           => 'synergia_member',
 	'order'          => 'ASC',
 	'orderby'        => 'post_count',
@@ -27,23 +27,44 @@ $args = array (
 			'key'       => 'post_count',
 			'compare'   => '>',
 			'type'      => 'NUMERIC',
+            'value'     => '0',
+		),
+	),
+);
+
+$administrator_args = array (
+	'role'           => 'administrator',
+	'order'          => 'ASC',
+	'orderby'        => 'post_count',
+	'meta_query'     => array(
+		array(
+			'key'       => 'post_count',
+			'compare'   => '>',
+			'type'      => 'NUMERIC',
+            'value'     => '0',
 		),
 	),
 );
 // Create the WP_User_Query object
-$wp_user_query = new WP_User_Query($args);
+$administrator_query = new WP_User_Query($administrator_args);
+$synergia_member_query = new WP_User_Query($synergia_member_args);
+
+$administrators = $administrator_query->get_results();
+$synergia_members = $synergia_member_query->get_results();
+
+$members = array_merge( $administrators, $synergia_members );
+
 // Get the results
-$authors = $wp_user_query->get_results();
 // Check for results
-if (!empty($authors))
+if (!empty($members))
 {
     echo '<ul>';
     // loop trough each author
-    foreach ($authors as $author)
+    foreach ($members as $member)
     {
         // get all the user's data
-        $author_info = get_userdata($author->ID);
-        echo '<li>'.$author_info->user_nicename.'</li>';
+        $member_info = get_userdata($member->ID);
+        echo '<li>'.$member_info->user_nicename.'</li>';
     }
     echo '</ul>';
 } else {
