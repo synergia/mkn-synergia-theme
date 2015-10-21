12d01d90ab48b65301e94d2ddfc93a4f2b9f96ec
stsdc <s.dac@yandex.ru>
2015-10-21 23:42:14 +0200

    Poprawiono krzyżyk na banerze #20

--- template-part-recruit.php
+++ template-part-recruit.php
@@ -5,7 +5,7 @@
     <i class="icon icon-user-add"></i>
   </label>
   <div class="modal__overlay" style="background-image: url('http://192.168.0.25/synergia/wp-content/uploads/2015/09/wallhaven-196335.jpg');">
-    <label for="modal"><i class="icon icon-cancel"></i></label>
+    <label for="modal"><i class="icon icon-close"></i></label>
 
     <div class="modal__box">
       <h2>Modal Title</h2>
