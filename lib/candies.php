b0e50b7821e6a45166db68acce24ed2940846def
stsdc <s.dac@yandex.ru>
2015-10-28 18:14:50 +0100

    Skrócono tytuł #23

--- lib/candies.php
+++ lib/candies.php
@@ -16,12 +16,6 @@ if (!function_exists('synergia_wp_title')) {
         // Add the site name.
         $title .= get_bloginfo('name');
 
-        // Add the site description for the home/front page.
-        $site_description = get_bloginfo('description', 'display');
-        if ($site_description && (is_home() || is_front_page())) {
-            $title = "$title $sep $site_description";
-        }
-
         // Add a page number if necessary.
         if ($paged >= 2 || $page >= 2) {
             $title = "$title $sep ".sprintf(__('Page %s', 'synergia'), max($paged, $page));
