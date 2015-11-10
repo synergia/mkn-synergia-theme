3f2ca57b0e95ea8da5dfd4e8d1fafc4fd86d6ba6
stsdc <s.dac@yandex.ru>
2015-11-10 23:54:57 +0100

    naprawiono layout autorów pod obrazkiem tytułowym projektu

--- template-part-authors.php
+++ template-part-authors.php
@@ -1,23 +1,33 @@
 <div class="authors gl">
 <?php //http://wordpress.stackexchange.com/a/119946
 global $post;
-$author_id=$post->post_author;
+$author_id = $post->post_author;
 project_counter();
 
-foreach( get_coauthors() as $curauth ):
+foreach (get_coauthors() as $curauth):
 ?>
 	<div class="co-author gl-cell gl-sm-1 gl-lg-3 gl-md-4">
-		<div class="userpic-project">
-			<a href="<?php echo get_author_posts_url( $curauth->ID, $curauth->user_nicename ); ?>">
-				<?php show_avatar($curauth);?>
-			</a>
-			<div class="project-count"><?php echo $curauth->project_count; ?></div>
-		</div>
-		<div>
-			<div class="gl-vertical gl">
-				<h3 class="gl-cell"><a href="<?php echo get_author_posts_url( $curauth->ID, $curauth->user_nicename ); ?>"  rel="author"><?php echo $curauth->display_name; ?></a></h3>
-				<div class="gl-cell">
-					<?php social_links($curauth); ?>
+		<div class="gl">
+			<div class="gl-cell gl-sm-3">
+				<div class="userpic-project">
+					<a href="<?php echo get_author_posts_url($curauth->ID, $curauth->user_nicename); ?>">
+						<?php show_avatar($curauth);?>
+					</a>
+					<div class="project-count"><?php echo $curauth->project_count; ?></div>
+				</div>
+			</div>
+			<div class="gl-cell">
+				<div class="gl-vertical gl">
+						<h3 class="gl-cell">
+							<a href="<?php echo get_author_posts_url($curauth->ID, $curauth->user_nicename); ?>"  rel="author">
+								<?php echo $curauth->display_name; ?>
+							</a>
+						</h3>
+					<div class="gl-cell">
+						<div class="icons-wrapper">
+							<?php social_links($curauth); ?>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
