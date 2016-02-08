b049f0d7075993b1406640701f51eef1f2d3d9c6
stsdc <s.dac@yandex.ru>
2016-02-08 23:44:58 +0100

    linki na stronie projektu -> BEM

--- template-part-authors.php
+++ template-part-authors.php
@@ -12,13 +12,13 @@ foreach (get_coauthors() as $curauth):
 					<a href="<?php echo get_author_posts_url($curauth->ID, $curauth->user_nicename); ?>">
 						<?php echo show_avatar($curauth);?>
 					</a>
-					<div class="authors__counter"><?php echo get_number_of_projects($curauth, 'finished') ?></div>
+					<div title="Liczba ukończonych projektów" class="authors__counter"><?php echo get_number_of_projects($curauth, 'finished') ?></div>
 				</div>
 			</div>
 			<div class="gl-cell">
 				<div class="gl-vertical gl">
 						<h3 class="gl-cell authors__name">
-							<a href="<?php echo get_author_posts_url($curauth->ID, $curauth->user_nicename); ?>"  rel="author"><?php echo $curauth->display_name; ?></a>
+							<a class="link link--name" href="<?php echo get_author_posts_url($curauth->ID, $curauth->user_nicename); ?>"  rel="author"><?php echo $curauth->display_name; ?></a>
 						</h3>
 					<div class="gl-cell">
 						<div class="authors__icons">
