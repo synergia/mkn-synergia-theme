777b4b79892bea118495274615b319f84ec8f859
stsdc <s.dac@yandex.ru>
2016-03-13 18:00:28 +0100

    osobny folder dla części

---
+++ parts/topbar.php
@@ -0,0 +1,23 @@
+<header class="topbarWrapper">
+    <div class="topbar container">
+        <a itemprop="url" class="topbar__logo link--logo" title="<?php bloginfo('name'); ?>" href="<?php echo site_url(); ?>" aria-label="Logo Synergii" role="presentation"></a>
+        <h1 itemprop="name" class="topbar__appname"><?php bloginfo('name'); ?></h1>
+
+        <nav class="nav" id="nav" role="navigation">
+          <?php wp_nav_menu(array(
+                        'theme_location' => 'main_menu',
+                        'container' => '',
+                        'menu_class' => 'nav__menu',
+                        'walker' => new Walker_Simple_Example(),
+                    )); ?>
+        </nav>
+      <div id="nav-trigger" class="topbar__trigger">
+        <div class="navicon-button x">
+          <div class="navicon"></div>
+        </div>
+      </div>
+      <!-- <nav id="nav-mobile"></nav> -->
+  </div>
+</header>
+<div class="global" data-ajax-url="<?php echo admin_url('admin-ajax.php'); ?>">
+  <div class="container">
