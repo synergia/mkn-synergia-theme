333d3ffc5379acecf930b157320429445a86a94b
stsdc <s.dac@yandex.ru>
2016-04-18 20:46:17 +0200

    ustawienia profilu  teraz zależne od Carbon Fields

--- lib/members/profile.php
+++ lib/members/profile.php
@@ -1,25 +1,27 @@
 <?php
-// Modyfikacje powiązane z kontem członka //
-
-
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
+// Modyfikacje powiązane z kontem członka //
 
-Container::make('user_meta', 'Zdjęcia i obrazki')
-    ->add_fields(array(
-        Field::make('image', 'crb_member_profile_image', 'Obrazek profilowy')
-            ->set_value_type('url')
-            ->set_width(50)
-            ->help_text('Można też wkleić URL'),
-        Field::make('image', 'crb_member_cover_image', 'Obrazek tła (można wkleić URL)')
-            ->set_value_type('url')
-            ->help_text('Można też wkleić URL')
-            ->set_width(50),
-    ));
-    $synergia_member = get_role('synergia_member');
+add_action('carbon_register_fields', 'crb_register_member_images');
+function crb_register_member_images()
+{
+    Container::make('user_meta', 'Zdjęcia i obrazki')
+        // ->show_on_user_role('synergia_member')
+        ->add_fields(array(
+            Field::make('image', 'crb_member_profile_image', 'Obrazek profilowy')
+                ->set_value_type('url')
+                ->help_text('Można też wkleić URL'),
+            Field::make('image', 'crb_member_cover_image', 'Obrazek tła')
+                ->set_value_type('url')
+                ->help_text('Można też wkleić URL')
+        ));
+
+}
+add_action('carbon_register_fields', 'crb_register_member_social_links');
+function crb_register_member_social_links() {
 
 Container::make('user_meta', 'Linki do portali społecznościowych, CV i poczta')
-    // ->show_on_user_role(array('administrator','synergia_member'))
     ->add_fields(array(
         Field::make('text', 'crb_member_github_link', 'URL Github'),
         Field::make('text', 'crb_member_facebook_link', 'URL Facebook'),
@@ -31,7 +33,12 @@ Container::make('user_meta', 'Linki do portali społecznościowych, CV i poczta'
             ->help_text('Zaznacz, jeśli chcesz, by był wyświetlany'),
 
     ));
-// if (current_user_can('level_10')) {
+}
+if (current_user_can('administrator')){
+    add_action('carbon_register_fields', 'crb_register_management_board');
+}
+function crb_register_management_board() {
+
     Container::make('user_meta', 'Obierz zarząd lub prezesa')
         ->add_fields(array(
             Field::make('radio', 'crb_managment_board', 'Mianuj na członka zarządu lub prezesa')
@@ -41,70 +48,6 @@ Container::make('user_meta', 'Linki do portali społecznościowych, CV i poczta'
                 'president' => 'Prezes',
             )),
         ));
-// }
-
+}
 //removing color scheme
 remove_action('admin_color_scheme_picker', 'admin_color_scheme_picker');
-
-// Checkbox dla prezesa
-
-function management_board($user)
-{
-    global $user_ID;
-    if ($user_ID && current_user_can('level_10')) {
-        ?>
-<h3>Obierz zarząd</h3>
-	<table class="form-table">
-		<tr>
-			<th><label>Obierz prezesa</label></th>
-			<td>
-        <label>
-          <input type="checkbox"
-                 name="president"
-                 id="president"
-                 value="yes"
-          <?php if (esc_attr(get_the_author_meta('president', $user->ID)) == true) {
-    echo 'checked';
-}
-        ?> />
-          Zaznacz, jeśli jest prezesem
-        </label>
-			</td>
-    </tr>
-    <tr>
-      <th><label>Obierz członka zarządu</label></th>
-      <td>
-        <label for="member_of_managment_board">
-          <input type="checkbox"
-                 name="member_of_managment_board"
-                 id="member_of_managment_board"
-                 value="yes"
-          <?php if (esc_attr(get_the_author_meta('member_of_managment_board', $user->ID)) == true) {
-    echo 'checked';
-}
-        ?> />
-				Zaznacz, jeśli jest członkiem zarządu
-      </label>
-			</td>
-		</tr>
-	</table>
-  <script>
-(function($) {
-  if($('#president').prop('checked') == true) {
-    $('#member_of_managment_board').prop('checked', false).attr("disabled", true);
-  }
-    $('#president').click(function() {
-      if ($(this).prop('checked') == true) {
-        console.log("click");
-      $('#member_of_managment_board').prop('checked', false).attr("disabled", true);
-    } else {
-      $('#member_of_managment_board').attr("disabled", false);
-    }
-  });
-
-})(jQuery);
-  </script>
-<?php
-
-    }
-}
--- lib/sponsors/utils.php
+++ lib/sponsors/utils.php
@@ -1,9 +1,16 @@
 <?php
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
 
 add_image_size( 'sponsor_logo', auto, 75, false );
 
-
-add_action('carbon_register_fields', 'crb_register_custom_fields');
-function crb_register_custom_fields() {
-    include_once(dirname(__FILE__) . '/link.php');
+add_action('carbon_register_fields', 'crb_sponsor_link');
+function crb_sponsor_link() {
+    Container::make('post_meta', 'Link')
+        ->show_on_post_type('sponsorowane')
+        ->set_context('side')
+        ->set_priority('high')
+        ->add_fields(array(
+            Field::make('text', 'crb_sponsor_link', 'Podaj link')
+        ));
 }
